0d04f8fdb57b38f4a27081d18fc79dd5313edd95
jprincip21 <jprincip21@gmail.com>
2025-12-08 22:11:02 -0500

    Moved Filtering Logic

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ import requests
 from dotenv import load_dotenv
 import os
 
-def get_data(place, forecast_days=None, category=None):
+def get_data(place, forecast_days=None):
 
     days = forecast_days * 8 # Days are broken into increments of 3 hours. One day = 8 points
 
@@ -12,22 +12,16 @@ def get_data(place, forecast_days=None, category=None):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&cnt={days}&units=metric&appid={API_KEY}"
 
     response = requests.get(url)
+
     if response.status_code == 200:
         data = response.json()
         print(data)
-
         filtered_data = data["list"]
-
-        if category == "Temperature":
-            filtered_data = [dictionary["main"]["temp"] for dictionary in filtered_data]
-
-        if category == "Sky":
-            filtered_data = [dictionary["weather"][0]["main"] for dictionary in filtered_data]
-
-        print(filtered_data)
         return filtered_data
+
+
     return None
 
 
 if __name__  == "__main__":
-    get_data(place="Tokyo", forecast_days=3, category="Temperature")
+    get_data(place="Tokyo", forecast_days=3)

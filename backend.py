0a9b2e3bb7fa9b9fab9fd32bad927a65e0855073
jprincip21 <jprincip21@gmail.com>
2025-12-08 21:45:36 -0500

    Added backend to pull from open weather map to get data by specified city

---
+++ backend.py
@@ -0,0 +1,33 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+def get_data(place, forecast_days=None, category=None):
+
+    days = forecast_days * 8 # Days are broken into increments of 3 hours. One day = 8 points
+
+    load_dotenv()
+    API_KEY = os.getenv("API_KEY")
+
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&cnt={days}&units=metric&appid={API_KEY}"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+
+        filtered_data = data["list"]
+
+        if category == "Temperature":
+            filtered_data = [dictionary["main"]["temp"] for dictionary in filtered_data]
+
+        if category == "Sky":
+            filtered_data = [dictionary["weather"][0]["main"] for dictionary in filtered_data]
+
+        print(filtered_data)
+        return filtered_data
+    return None
+
+
+if __name__  == "__main__":
+    get_data(place="Tokyo", forecast_days=3, category="Temperature")
